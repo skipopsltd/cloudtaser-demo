92797f156d28f7dea2f25f35648371f72d77450a
yellowmegaman <yellowmegaman@gmail.com>
2026-03-16 14:44:38 +0100

    Combine foreground script into single line

--- demo/foreground.sh
+++ demo/foreground.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-echo "Setting up CloudTaser demo environment..."
-echo "This takes about 2 minutes."
-echo ""
-while [ ! -f /tmp/.cloudtaser-setup-done ]; do sleep 2; done
-echo "Ready! Click Next to begin."
+echo "Setting up (~2 min)..." && while [ ! -f /tmp/.cloudtaser-setup-done ]; do sleep 2; done && echo "Ready!"
